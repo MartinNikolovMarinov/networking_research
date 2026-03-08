83b7a879375fcf1ec3118890130452574fd7da64
MartinNikolovMarinov <martin_n_marinov@protonmail.com>
2026-03-08 15:20:43 +0200

    list network interfaces

--- sandbox.cpp
+++ sandbox.cpp
@@ -3,188 +3,232 @@
 #include "common_types.h"
 #include "diagnostics/diagnostics.h" // IWYU pragma: keep
 
+#include <fcntl.h>
 #include <sys/socket.h>
 #include <linux/netlink.h>
 #include <linux/rtnetlink.h>
+#include <poll.h>
 #include <unistd.h>
 
-// core::expected<sp::OsErr> checkNetlinkHeader(const nlmsghdr* nh) {
-//     if (nh->nlmsg_type == NLMSG_ERROR)
-//     {
-//         const nlmsgerr* nherr = reinterpret_cast<const nlmsgerr*>(NLMSG_DATA(nh));
-//         if (nherr->error == 0) {
-//             return {}; // Is this actually something possible ?
-//         }
-//         i32 code = -nherr->error;
-//         RETURN_OS_ERR(code);
-//     }
+namespace sp {
 
-//     return {};
-// }
-
-// struct GetInfoNetInterfaceMessage {
-//     struct nlmsghdr nlh;
-//     struct ifinfomsg ifm;
-// };
-
-// struct Netlink {
-//     core::AtomicU32 seq;
-//     i32 fd;
-
-//     Netlink() : seq(0), fd(-1) {}
-//     Netlink(core::AtomicU32 _seq, i32 _fd) : seq(_seq.load()), fd(_fd) {}
-//     Netlink(Netlink&& other) {
-//         this->seq = other.seq.load();
-//         this->fd = other.fd;
-
-//         other.seq = 0;
-//         other.fd = -1;
-//     }
-
-//     // core::expected<sp::OsErr> getNetworkInterfaceInfo() {
-//     //     GetInfoNetInterfaceMessage payload = {};
-
-//     //     payload.nlh.nlmsg_len = NLMSG_LENGTH(sizeof(struct ifinfomsg));
-//     //     payload.nlh.nlmsg_type = RTM_GETLINK;
-//     //     payload.nlh.nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP;
-//     //     payload.nlh.nlmsg_seq = seq.fetch_add(1);
-
-//     //     payload.ifm.ifi_family = AF_PACKET;
-
-//     //     addr_off sendN = send(fd, &payload, payload.nlh.nlmsg_len, 0);
-//     //     TRY_SYSCALL(sendN > 0);
-
-//     //     msghdr msghdr = {};
-//     //     iovec iov;
-//     //     // char buf[8192];
-//     //     char buf[100];
-
-//     //     iov.iov_base = buf;
-//     //     iov.iov_len  = sizeof(buf);
-
-//     //     msghdr.msg_iov = &iov;
-//     //     msghdr.msg_iovlen = 1;
-
-//     //     bool done = false;
-//     //     while (!done) {
-//     //         addr_off recvMsgN = recvmsg(fd, &msghdr, 0);
-//     //         TRY_SYSCALL(recvMsgN >= 0);
-
-//     //         nlmsghdr *nh = reinterpret_cast<nlmsghdr*>(buf);
-
-//     //         while(NLMSG_OK(nh, recvMsgN)) {
-//     //             if (nh->nlmsg_type == NLMSG_DONE) break;
-//     //             TRY_EXPR(checkNetlinkHeader(nh));
-
-//     //             ifinfomsg* ifi = reinterpret_cast<ifinfomsg*>(NLMSG_DATA(nh));
-
-//     //             rtattr *attr = IFLA_RTA(ifi);
-//     //             u32 attrLen = nh->nlmsg_len - NLMSG_LENGTH(sizeof(*ifi));
-
-//     //             while (RTA_OK(attr, attrLen)) {
-//     //                 if (attr->rta_type == IFLA_IFNAME) {
-//     //                     logInfo("intrface: {}", reinterpret_cast<const char*>(RTA_DATA(attr)));
-//     //                 }
-//     //                 attr = RTA_NEXT(attr, attrLen);
-//     //             }
-
-//     //             nh = NLMSG_NEXT(nh, recvMsgN);
-//     //         }
-//     //     }
-
-//     //     return {};
-//     // }
-
-//     core::expected<sp::OsErr> close() {
-//         i32 closeRes = ::close(this->fd);
-//         TRY_SYSCALL(closeRes >= 0);
-//         seq = 0;
-//         return {};
-//     }
-
-//     static core::expected<Netlink, sp::OsErr> create() {
-//         i32 fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
-//         TRY_SYSCALL(fd >= 0);
-
-//         struct sockaddr_nl addr = {};
-//         addr.nl_family = AF_NETLINK;
-//         addr.nl_pid = u32(getpid());
-
-//         i32 bindRes = bind(fd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
-//         TRY_SYSCALL(bindRes >= 0);
-
-//         Netlink ret (0, fd);
-//         return ret;
-//     }
-// };
-
-// core::expected<sp::OsErr> _main() {
-//     Netlink netlink = TRY(Netlink::create());
-//     defer { netlink.close(); };
-
-//     // addr_off sendN = send(fd, &req, req.nlh.nlmsg_len, 0);
-//     // TRY_SYSCALL(sendN > 0);
-
-//     // char buff[1024] = {};
-
-//     // addr_off recvN = recv(fd, buff, sizeof(buff), 0);
-//     // TRY_SYSCALL(recvN > 0);
-
-//     // const nlmsghdr* nh = reinterpret_cast<const nlmsghdr*>(buff);
-//     // TRY_EXPR(checkNetlinkHeader(nh));
-
-//     // while(NLMSG_OK(nh, recvN)) {
-//     //     if (nh->nlmsg_type == NLMSG_DONE) break;
-//     //     TRY_EXPR(checkNetlinkHeader(nh));
-
-//     //     ifinfomsg* ifi = reinterpret_cast<ifinfomsg*>(NLMSG_DATA(nh));
-
-//     //     rtattr *attr = IFLA_RTA(ifi);
-//     //     u32 attrLen = nh->nlmsg_len - NLMSG_LENGTH(sizeof(*ifi));
-
-//     //     while (RTA_OK(attr, attrLen)) {
-//     //         if (attr->rta_type == IFLA_IFNAME) {
-//     //             logInfo("intrface: {}", reinterpret_cast<const char*>(RTA_DATA(attr)));
-//     //         }
-//     //         attr = RTA_NEXT(attr, attrLen);
-//     //     }
-
-//     //     nh = NLMSG_NEXT(nh, recvN);
-//     // }
-
-//     // i32 closeRes = close(fd);
-//     // TRY_SYSCALL(closeRes >= 0);
-
-//     return {};
-// }
-
-core::expected<i32, sp::OsErr> ex1_createOsErrorNoDiag(bool hasErr) {
-    if (hasErr) {
-        [[maybe_unused]] sp::OsErr a = CREATE_OS_ERR(-1);
-        sp::OsErr b = CREATE_OS_ERR(-2);
-        return core::unexpected(std::move(b));
+core::expected<sp::OsErr> checkNetlinkHeader(const nlmsghdr* nh) {
+    if (nh->nlmsg_type == NLMSG_ERROR)
+    {
+        const nlmsgerr* nherr = reinterpret_cast<const nlmsgerr*>(NLMSG_DATA(nh));
+        if (nherr->error == 0) {
+            return {}; // Is this actually something possible ?
+        }
+        i32 code = -nherr->error;
+        RETURN_OS_ERR(code);
     }
-    return 1;
+
+    return {};
 }
 
-core::expected<i32, sp::OsErr> ex2_l2_returnOsErrWithDiag(bool hasErr) {
-    if (hasErr) {
-        RETURN_OS_ERR(10);
+struct GetInfoNetInterfacePayload {
+    nlmsghdr nlh;
+    ifinfomsg ifm;
+
+    static GetInfoNetInterfacePayload create(u32 seq) {
+        GetInfoNetInterfacePayload payload = {};
+
+        payload.nlh.nlmsg_len = NLMSG_LENGTH(sizeof(ifinfomsg));
+        payload.nlh.nlmsg_type = RTM_GETLINK;
+        payload.nlh.nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP;
+        payload.nlh.nlmsg_seq = seq;
+
+        payload.ifm.ifi_family = AF_PACKET;
+
+        return payload;
     }
-    return 1;
-}
+};
 
-core::expected<i32, sp::OsErr> ex2_l1_returnOsErrWithDiag(bool hasErr) {
-    TRYX(ex2_l2_returnOsErrWithDiag(hasErr));
-    return 1;
-}
+struct Netlink {
+    core::AtomicU32 seq;
+    i32 fd;
+
+    Netlink() : seq(0), fd(-1) {}
+    Netlink(core::AtomicU32 _seq, i32 _fd) : seq(_seq.load()), fd(_fd) {}
+    Netlink(Netlink&& other) {
+        this->seq = other.seq.load();
+        this->fd = other.fd;
+
+        other.seq = 0;
+        other.fd = -1;
+    }
+
+    core::expected<sp::OsErr> waitForReadable(i32 timeoutMs) {
+        pollfd pfd = {};
+        pfd.fd = fd;
+        pfd.events = POLLIN;
+
+        i32 pollRes = poll(&pfd, 1, timeoutMs);
+        TRY_SYSCALL(pollRes >= 0);
+
+        if (pollRes == 0) {
+            RETURN_OS_ERR(ETIMEDOUT);
+        }
+
+        if ((pfd.revents & (POLLERR | POLLHUP | POLLNVAL)) != 0) {
+            RETURN_OS_ERR(EIO);
+        }
+
+        if ((pfd.revents & POLLIN) == 0) {
+            RETURN_OS_ERR(EAGAIN);
+        }
+
+        return {};
+    }
+
+    template <core::AllocatorId TAllocId = core::DEFAULT_ALLOCATOR_ID>
+    core::expected<sp::OsErr> getNetworkInterfaceInfo(core::ArrList<core::StrBuilder<TAllocId>, TAllocId>& out, i32 timeoutMs) {
+        u32 sequence = this->seq.fetch_add(1);
+        auto payload = GetInfoNetInterfacePayload::create(sequence);
+
+        addr_off sendN = send(fd, &payload, payload.nlh.nlmsg_len, 0);
+        TRY_SYSCALL(sendN > 0);
+
+        auto onMessage = [&out](nlmsghdr* currNh) -> core::expected<sp::OsErr> {
+            ifinfomsg* ifi = reinterpret_cast<ifinfomsg*>(NLMSG_DATA(currNh));
+
+            if (currNh->nlmsg_len < NLMSG_LENGTH(sizeof(*ifi))) {
+                RETURN_OS_ERR(EBADMSG);
+            }
+
+            u8* attrCursor = reinterpret_cast<u8*>(ifi) + NLMSG_ALIGN(sizeof(ifinfomsg));
+            u32 attrLen = currNh->nlmsg_len - NLMSG_LENGTH(sizeof(*ifi));
+
+            while (attrLen >= sizeof(rtattr)) {
+                rtattr* attr = reinterpret_cast<rtattr*>(attrCursor);
+                if (attr->rta_len < sizeof(rtattr) || attr->rta_len > attrLen) {
+                    RETURN_OS_ERR(EBADMSG);
+                }
+
+                if (attr->rta_type == IFLA_IFNAME) {
+                    const char* ifname = reinterpret_cast<const char*>(RTA_DATA(attr));
+                    out.push(core::sv(ifname));
+                }
+
+                u32 step = RTA_ALIGN(attr->rta_len);
+                attrCursor += step;
+                attrLen -= step;
+            }
+
+            return {};
+        };
+
+        TRYX(receiveUntilDone(sequence, timeoutMs, onMessage));
+
+        return {};
+    }
+
+    core::expected<sp::OsErr> close() {
+        this->seq = 0;
+        i32 closeRes = ::close(this->fd);
+        TRY_SYSCALL(closeRes >= 0);
+        return {};
+    }
+
+    static core::expected<Netlink, sp::OsErr> create() {
+        // Create netlink socket for routing:
+        i32 fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
+        TRY_SYSCALL(fd >= 0);
+
+        // Configure the socket to be NON blocking.
+        i32 currentFlags = fcntl(fd, F_GETFL);
+        TRY_SYSCALL(currentFlags >= 0);
+        i32 setFlagsRes = fcntl(fd, F_SETFL, currentFlags | O_NONBLOCK);
+        TRY_SYSCALL(setFlagsRes >= 0);
+
+        struct sockaddr_nl addr = {};
+        addr.nl_family = AF_NETLINK;
+        addr.nl_pid = u32(getpid());
+
+        i32 bindRes = bind(fd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
+        TRY_SYSCALL(bindRes >= 0);
+
+        Netlink ret (0, fd);
+        return ret;
+    }
+
+private:
+
+    template <typename TOnMessageFn>
+    core::expected<sp::OsErr> receiveUntilDone(u32 sequence, i32 timeoutMs, TOnMessageFn&& onMessage) {
+        u8 msgBuf[8192]; // TODO: [SLAB_ALLOCATOR] use slab allocator.
+
+        iovec iov;
+        iov.iov_base = msgBuf;
+        iov.iov_len = sizeof(msgBuf);
+
+        msghdr msg = {};
+        msg.msg_iov = &iov;
+        msg.msg_iovlen = 1;
+
+        bool done = false;
+        while (!done) {
+            TRYX(waitForReadable(timeoutMs));
+
+            msg.msg_flags = 0;
+            addr_off recvMsgN = recvmsg(fd, &msg, 0);
+            if (recvMsgN < 0) {
+                if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                    continue;
+                }
+                RETURN_OS_ERR(errno);
+            }
+            if ((msg.msg_flags & MSG_TRUNC) != 0) {
+                RETURN_OS_ERR(EMSGSIZE);
+            }
+
+            u32 msgLen = u32(recvMsgN);
+            u8* cursor = msgBuf;
+
+            while (msgLen >= sizeof(nlmsghdr)) {
+                nlmsghdr* currNh = reinterpret_cast<nlmsghdr*>(cursor);
+                if (currNh->nlmsg_len < sizeof(nlmsghdr) || currNh->nlmsg_len > msgLen) {
+                    RETURN_OS_ERR(EBADMSG);
+                }
+
+                if (currNh->nlmsg_seq != sequence) {
+                    u32 step = NLMSG_ALIGN(currNh->nlmsg_len);
+                    cursor += step;
+                    msgLen -= step;
+                    continue;
+                }
+
+                if (currNh->nlmsg_type == NLMSG_DONE) {
+                    done = true;
+                    break;
+                }
+
+                TRYX(checkNetlinkHeader(currNh));
+                TRYX(onMessage(currNh));
+
+                u32 step = NLMSG_ALIGN(currNh->nlmsg_len);
+                cursor += step;
+                msgLen -= step;
+            }
+        }
+
+        return {};
+    }
+};
+
+} // namespace sp
 
 core::expected<sp::OsErr> _main() {
-    i32 x = TRYV(ex1_createOsErrorNoDiag(false));
-    logInfo("x={}", x);
+    sp::Netlink netlink = TRYV(sp::Netlink::create());
+    defer { netlink.close(); };
 
-    TRYX(ex2_l1_returnOsErrWithDiag(true));
-    logInfo("passed");
+    core::ArrList<core::StrBuilder<>> out;
+    TRYX(netlink.getNetworkInterfaceInfo(out, 3000));
+
+    for (addr_size i = 0; i < out.len(); i++) {
+        auto& x = out[i];
+        logInfo("interface: {}", x.view());
+    }
 
     return {};
 }
